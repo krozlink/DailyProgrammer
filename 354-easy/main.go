b9a227755e8a05d87f6e68b0cf6e0fdbbda1cc0c
Daniel Taylor <dmtaylor17@gmail.com>
2018-03-18 21:48:47 +1000

    Minor changes. Tidied readme

--- 354-easy/main.go
+++ 354-easy/main.go
@@ -11,29 +11,32 @@ func main() {
 	fmt.Println(Solve(input))
 }
 
+// Solve - Given a number A, find the smallest possible value of B+C, if B*C = A
 func Solve(input uint64) uint64 {
-	lowest := uint64(input + 1)
+	//input + 1 will always be a valid answer so make this the default
+	solution := uint64(input + 1)
+
 	factors := factor(input)
+	sort.Slice(factors, func(i, j int) bool { return factors[i] < factors[j] })
+
 	for i := 0; i < len(factors)/2; i++ {
 		x, y := factors[i], factors[len(factors)-1-i]
-		if x+y < lowest {
-			lowest = x + y
+		if x+y < solution {
+			solution = x + y
 		}
 	}
-	return lowest
+	return solution
 }
 
 func factor(value uint64) []uint64 {
 	factors := make([]uint64, 0)
 
 	sqrt := math.Sqrt(float64(value))
-	ceiling := uint64(math.Ceil(sqrt))
+	ceiling := uint64(math.Floor(sqrt))
 	for i := uint64(1); i <= ceiling; i++ {
 		if value%i == 0 {
 			factors = append(factors, i, value/i)
 		}
 	}
-
-	sort.Slice(factors, func(i, j int) bool { return factors[i] < factors[j] })
 	return factors
 }
